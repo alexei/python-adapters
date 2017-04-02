468082e03895806bdac63e9f301f40f9088028f7
Alexei <hello@alexei.ro>
2017-04-02 20:43:15 +0300

    Fix import issues

---
+++ adapter/adapter.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+
+__all__ = ['Adapter', 'CharField']
+
+
+class CharField(object):
+    pass
+
+
+class Adapter(object):
+    pass
